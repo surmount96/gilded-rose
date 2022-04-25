85a201e8671545ab0a1bfe0f47ddc8beef923727
surmount96 <ayanwoye74@gmail.com>
2022-04-25 21:07:16 +0100

    Once the sell by date has passed, Quality degrades twice as fast

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -11,7 +11,7 @@ function update_quality() {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
+          items[i].quality = items[i].quality - 2
         }
       }
     } else {
