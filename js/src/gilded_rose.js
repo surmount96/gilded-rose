735fc9123262fbb9c72845cb0e9f8b93f4af8601
surmount96 <ayanwoye74@gmail.com>
2022-04-26 21:17:45 +0100

    Quality base and ensure it doesnt decrease by 1 after selling day passed

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -9,7 +9,7 @@ var items = []
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0 ) {
+      if (items[i].quality > 0 && items[i].sell_in > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
           items[i].quality = items[i].quality - 1;
         }
